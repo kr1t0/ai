13cd8f93f56beb1a6568e41bf28ce74f62838cdf
Even Roll <1635891753@qq.com>
2024-09-13 11:23:41 +0800

    refine the bilinear interpolation

--- Even/cv03/binary_interp.py
+++ Even/cv03/binary_interp.py
@@ -10,24 +10,24 @@ def function(img, parm):
     for i in range(channel):
         for dst_x in range(dst_h):
             for dst_y in range(dst_w):
-                src_x = (dst_x + 0.5) / scale_x -0.5
-                src_y = (dst_y+0.5) / scale_y - 0.5
+                src_x = (dst_x + 0.5) / scale_x - 0.5
+                src_y = (dst_y + 0.5) / scale_y - 0.5
 
                 src_x0 = int(np.floor(src_x))
                 src_x1 = min(src_x0 + 1, src_h - 1)
                 src_y0 = int(np.floor(src_y))
-                src_y1 = min(src_y0, src_w - 1)
+                src_y1 = min(src_y0 + 1, src_w - 1)
 
-                temp0 = (src_x - src_x0)* img[src_x1][src_y0][i] + (src_x1-src_x)*img[src_x0][src_y0][i]
-                temp1 = (src_x - src_x0)* img[src_x1][src_y1][i] + (src_x1-src_x)*img[src_x0][src_y1][i]
+                temp0 = (src_x - src_x0) * img[src_x1][src_y0][i] + (src_x1-src_x) * img[src_x0][src_y0][i]
+                temp1 = (src_x - src_x0) * img[src_x1][src_y1][i] + (src_x1-src_x) * img[src_x0][src_y1][i]
 
-                dst_img[dst_x][dst_y][i] = int((src_x1-src_x)*temp0 + (src_x - src_x0)*temp1)
+                dst_img[dst_x][dst_y][i] = int((src_y1-src_y) * temp0 + (src_y - src_y0) * temp1)
 
     return dst_img
 
 img = cv2.imread("my.png")
 
-zoom = function(img, (600,600,3))
+zoom = function(img, (300,600,3))
 
 cv2.imshow("binary",zoom)
 cv2.imshow("src",img)
